4fae9d6b12051a385f3da404e73b132a837a6d2d
srinimogs <109311142+srinimogs@users.noreply.github.com>
2022-09-13 23:18:51 +0100

    add new code files
    these are files are part of week 1 practical activity 1.2.5

---
+++ first_practical_code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# this is my first line of code
+print ("My Name is Srinivas Moghe")
+print ("The Date is 13 Sep 2022")
+
+
+# In[10]:
+
+
+# Write a program to analyse real estate data
+# Total Sales for December
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+TotalSales = a + b + c + d + e
+print (TotalSales)
+
+
+# In[12]:
+
+
+# average sales price for December
+Average = TotalSales/5
+print (int(Average))
+
+
+# In[13]:
+
+
+# price for property C
+price = 67000 - 6500
+print (price)
+
+
+# In[ ]:
+
+
+
+
